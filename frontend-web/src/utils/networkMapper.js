128925a091177a6817a9f48affe8359772cf2510
DOBNA <dobna.co@gmail.com>
2026-07-08 19:24:01 +0330

    Update networkMapper.js

--- frontend-web/src/utils/networkMapper.js
+++ frontend-web/src/utils/networkMapper.js
@@ -1,1 +1,11 @@
-[]
+export const SUPPORTED_NETWORKS = {
+  BSC: { label: 'BSC (BEP20)', icon: 'bsc.svg', chain: 'BNB Smart Chain' },
+  TRON: { label: 'TRON (TRC20)', icon: 'tron.svg', chain: 'TRON' },
+  ETH: { label: 'Ethereum (ERC20)', icon: 'eth.svg', chain: 'Ethereum/ERC-20' },
+  PLASMA: { label: 'Plasma', icon: 'plasma.svg', chain: 'Plasma' },
+  SOLANA: { label: 'Solana', icon: 'solana.svg', chain: 'Solana' },
+  AVAX: { label: 'Avalanche C-Chain', icon: 'avax.svg', chain: 'Avalanche C-Chain' },
+  TON: { label: 'TON', icon: 'ton.svg', chain: 'The Open Network (TON)' },
+  POLKADOT: { label: 'Polkadot Asset Hub', icon: 'dot.svg', chain: 'Polkadot Asset Hub' },
+  CSC: { label: 'CSC', icon: 'csc.svg', chain: 'CoinEx Smart Chain' },
+};
