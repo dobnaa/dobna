fb316c092c4f16d20abb18d69b138acf6d9d5369
DOBNA <dobna.co@gmail.com>
2026-07-09 18:25:19 +0330

    Update main.jsx

--- frontend-web/src/main.jsx
+++ frontend-web/src/main.jsx
@@ -1,1 +1,14 @@
-[]
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './styles/index.css';
+import './utils/i18n'; // اگر i18n رو فعال می‌کنید
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
