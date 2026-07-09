de4bbc9472c3c201edbef2791544fc51dcc34576
DOBNA <dobna.co@gmail.com>
2026-07-09 18:25:45 +0330

    Update App.jsx

--- frontend-web/src/App.jsx
+++ frontend-web/src/App.jsx
@@ -1,1 +1,13 @@
-[]
+import { Suspense } from 'react';
+import AppRoutes from './routes/AppRoutes';
+import LoadingSpinner from './components/common/LoadingSpinner';
+
+function App() {
+  return (
+    <Suspense fallback={<LoadingSpinner />}>
+      <AppRoutes />
+    </Suspense>
+  );
+}
+
+export default App;
