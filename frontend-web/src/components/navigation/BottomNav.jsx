8d27de9bfca5e6142b57b6db91b0eb88e036d696
DOBNA <dobna.co@gmail.com>
2026-07-12 14:37:29 +0330

    Update BottomNav.jsx

--- frontend-web/src/components/navigation/BottomNav.jsx
+++ frontend-web/src/components/navigation/BottomNav.jsx
@@ -1,1 +1,15 @@
-[]
+// components/navigation/BottomNav.jsx
+import { Home, Wallet, Send, RefreshCw, Trophy, Sword } from 'lucide-react';
+
+const BottomNav = () => {
+  return (
+    <div className="flex justify-around p-4">
+      <Home className="w-6 h-6 text-white" />
+      <Wallet className="w-6 h-6 text-white" />
+      <Send className="w-6 h-6 text-white" />
+      <RefreshCw className="w-6 h-6 text-white" />
+      <Trophy className="w-6 h-6 text-white" />
+      <Sword className="w-6 h-6 text-white" />
+    </div>
+  );
+};
