c8b720620949bb3175bfda80043530b581c6680b
DOBNA <dobna.co@gmail.com>
2026-07-13 16:34:25 +0330

    Update i18n.js

--- frontend-web/src/utils/i18n.js
+++ frontend-web/src/utils/i18n.js
@@ -1,11 +1,12 @@
+// frontend-web/src/utils/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
-// فایل‌های زبان (در پوشه locales)
 import en from '../locales/en.json';
 import fa from '../locales/fa.json';
 import tr from '../locales/tr.json';
+import ar from '../locales/ar.json';
 
 i18n
   .use(LanguageDetector)
@@ -15,6 +16,7 @@ i18n
       en: { translation: en },
       fa: { translation: fa },
       tr: { translation: tr },
+      ar: { translation: ar },
     },
     fallbackLng: 'en',
     detection: {
