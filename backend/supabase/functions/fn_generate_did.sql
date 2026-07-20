c1d52668e0b7ad7060f098616f03bf5ad421215b
DOBNA <dobna.co@gmail.com>
2026-07-21 01:42:01 +0330

    Update fn_generate_did.sql

--- backend/supabase/functions/fn_generate_did.sql
+++ backend/supabase/functions/fn_generate_did.sql
@@ -1,1 +1,10 @@
-[]
+-- تولید DID ۱۶ رقمی
+CREATE OR REPLACE FUNCTION fn_generate_did(p_prefix VARCHAR(1) DEFAULT '9')
+RETURNS VARCHAR(16) AS $$
+DECLARE
+    v_timestamp VARCHAR(15);
+BEGIN
+    v_timestamp := to_char(NOW(), 'YYMMDDHH24MISSMS');
+    RETURN p_prefix || v_timestamp;
+END;
+$$ LANGUAGE plpgsql;
