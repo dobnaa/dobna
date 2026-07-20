258d67b8feb5f2bb94f1abf27da7210b887d93f1
DOBNA <dobna.co@gmail.com>
2026-07-21 01:59:38 +0330

    Update fn_unfollow_user.sql

--- backend/supabase/functions/fn_unfollow_user.sql
+++ backend/supabase/functions/fn_unfollow_user.sql
@@ -1,1 +1,11 @@
-[]
+-- لغو دنبال کردن یک کاربر
+CREATE OR REPLACE FUNCTION fn_unfollow_user(
+    p_follower_id UUID,
+    p_following_id UUID
+)
+RETURNS VOID AS $$
+BEGIN
+    DELETE FROM public.followers
+    WHERE follower_id = p_follower_id AND following_id = p_following_id;
+END;
+$$ LANGUAGE plpgsql;
