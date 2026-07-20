6f25aa079c16849a6ea08d13ba28486a9dac4876
DOBNA <dobna.co@gmail.com>
2026-07-21 01:56:57 +0330

    Update fn_start_room_game.sql

--- backend/supabase/functions/fn_start_room_game.sql
+++ backend/supabase/functions/fn_start_room_game.sql
@@ -1,1 +1,7 @@
-[]
+-- شروع بازی در اتاق
+CREATE OR REPLACE FUNCTION fn_start_room_game(p_room_id BIGINT)
+RETURNS VOID AS $$
+BEGIN
+    PERFORM fn_start_game(p_room_id);
+END;
+$$ LANGUAGE plpgsql;
