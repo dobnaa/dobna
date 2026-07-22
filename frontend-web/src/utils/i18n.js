b3d02f5372bcb5c0c5c4bd3d3cae308b361ce8d5
DOBNA <dobna.co@gmail.com>
2026-07-23 01:20:27 +0330

    Update i18n.js

--- frontend-web/src/utils/i18n.js
+++ frontend-web/src/utils/i18n.js
@@ -1,4 +1,4 @@
-// frontend-web/src/utils/i18n.js
+// utils/i18n.js
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
@@ -7,24 +7,34 @@ import en from '../locales/en.json';
 import fa from '../locales/fa.json';
 import tr from '../locales/tr.json';
 import ar from '../locales/ar.json';
+import ru from '../locales/ru.json';
+import in from '../locales/in.json';
+import fr from '../locales/fr.json';
+import ch from '../locales/ch.json';
+
+const resources = {
+  en: { translation: en },
+  fa: { translation: fa },
+  tr: { translation: tr },
+  ar: { translation: ar },
+  ru: { translation: ru },
+  in: { translation: in },
+  fr: { translation: fr },
+  ch: { translation: ch },
+};
 
 i18n
   .use(LanguageDetector)
   .use(initReactI18next)
   .init({
-    resources: {
-      en: { translation: en },
-      fa: { translation: fa },
-      tr: { translation: tr },
-      ar: { translation: ar },
-    },
+    resources,
     fallbackLng: 'en',
+    interpolation: {
+      escapeValue: false,
+    },
     detection: {
       order: ['localStorage', 'navigator'],
       caches: ['localStorage'],
-    },
-    interpolation: {
-      escapeValue: false,
     },
   });
 
